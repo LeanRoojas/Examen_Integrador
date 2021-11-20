a3057c8abbb6f84b118cafd09583453165d8ddbe
Leandro Rojas <leandrorojas.educacion@gmail.com>
2021-11-19 23:51:52 -0300

    Consignas principales terminadas, ahora a jugar un poco :D

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ window.addEventListener("DOMContentLoaded",()=>{
    const orderbuttons = document.querySelectorAll("button[data-order-caracters]");
    orderbuttons.forEach(item =>{
        item.addEventListener("click",(event)=>{
+           
             const button = event.currentTarget;
             const container = button.parentNode.parentNode;
             const img = container.querySelector("img");
@@ -14,11 +15,26 @@ window.addEventListener("DOMContentLoaded",()=>{
                 id: button.getAttribute("data-order-caracters")
             }
             localStorage.setItem("card",JSON.stringify(carta));
-            // const url = window.location.href.replace("index.html","order.html");
-            // window.location.href = url;
+             
        })
    })
 
+   //Obtenemos las gorras, guardamos en localStorage y nos envia al formulario
+   const hats = document.querySelectorAll("button[data-order-hat]");
+   hats.forEach(hat => {
+       hat.addEventListener("click",(event)=>{
+            const button = event.currentTarget;
+            const container = button.parentNode;
+            const img = container.querySelector("img");
+            let hat = {
+                titulo: container.querySelector("p").innerText,
+                img: img.getAttribute("src")
+            }
+            localStorage.setItem("hat",JSON.stringify(hat));
+            const url = window.location.href.replace("index.html","order.html");
+             window.location.href = url;
+        })
+   })
 
 
    
--- order.js
+++ order.js
@@ -8,11 +8,25 @@ window.addEventListener("DOMContentLoaded",()=>{
         console.log(titulo,img,description);
         const cardCont = document.querySelector(".card-selection");
 
+        //Inserta el elemento en un container predeterminado
         cardCont.innerHTML = `
             <h1>${titulo}</h1>
             <img src="${img}" width=50">
             <p>${description}</p>
         `;
-
     }
+    const hat = localStorage.getItem("hat");
+    if(hat){
+        let selectedHat = JSON.parse(hat);
+        console.log(hat);
+        let {titulo,img} = selectedHat;
+        let hatCont = document.querySelector(".hat-selection");
+        hatCont.innerHTML = `
+            <h1>${titulo}</h1>
+            <img src="${img}">
+        `;
+    }
+
+
+
 })
