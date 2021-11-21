01d106a6f5470617a351552570c1e78cb5fde872
Leandro Rojas <leandrorojas.educacion@gmail.com>
2021-11-21 14:21:02 -0300

    Todo listo, pero podria estar mejor :v

--- order.js
+++ order.js
@@ -1,35 +1,28 @@
-window.addEventListener("DOMContentLoaded",()=>{
-    const card = localStorage.getItem("card");
-    if(card){
-        const selectedCard = JSON.parse(card);
-        console.log(card)
-        const {titulo,img,description} = selectedCard;
+window.addEventListener("DOMContentLoaded", () => {
+  const card = localStorage.getItem("card");
+  if (card) {
+    const selectedCard = JSON.parse(card);
+    const { titulo, img, description } = selectedCard;
+    const cardCont = document.querySelector(".card-selection");
 
-        console.log(titulo,img,description);
-        const cardCont = document.querySelector(".card-selection");
-
-        //Inserta el elemento en un container predeterminado
-        cardCont.innerHTML = `
+    //Inserta el elemento en un container predeterminado
+    cardCont.innerHTML = `
             <h1>${titulo}</h1>
             <img src="${img}" width=50">
             <p>${description}</p>
         `;
-    }
-    const hat = localStorage.getItem("hat");
-    if(hat){
-        let selectedHat = JSON.parse(hat);
-        console.log(hat);
-        let {titulo,img} = selectedHat;
-        let hatCont = document.querySelector(".hat-selection");
-        hatCont.innerHTML = `
+  }
+  const hat = localStorage.getItem("hat");
+  if (hat) {
+    let selectedHat = JSON.parse(hat);
+    let { titulo, img } = selectedHat;
+    let hatCont = document.querySelector(".hat-selection");
+    hatCont.innerHTML = `
             <h1>${titulo}</h1>
             <img src="${img}">
         `;
-    }
+  }
 
-    localStorage.removeItem("hat");
-    localStorage.removeItem("card");
-
-
-
-})
+  localStorage.removeItem("hat");
+  localStorage.removeItem("card");
+});
